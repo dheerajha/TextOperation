8c18ceacdb6d40f6bd2e8b9f152ca563ffd18a52
Dheeraj <jhadheeraj720@gamil.com>
2023-12-18 23:25:54 +0530

    this u=it the all about the prop vid 6

---
+++ src/components/Navbar.js
@@ -0,0 +1,67 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default function Navbar(props) {
+    // we are passing the props and passing the props.title and in the title we are doing {props.title}and in the title we are doing props.title and {} because it is the javaScript
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="/">
+                {props.title}
+            </a>
+            <button
+                className="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="/">
+                            Home <span className="sr-only">(current)</span>
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/">
+                            {props.aboutText}
+                        </a>
+                    </li>
+                </ul>
+                <form className="form-inline my-2 my-lg-0">
+                    <input
+                        className="form-control mr-sm-2"
+                        type="search"
+                        placeholder="Search"
+                        aria-label="Search"
+                    />
+                    <button
+                        className="btn btn-outline-success my-2 my-sm-0"
+                        type="submit"
+                    >
+                        Search
+                    </button>
+                </form>
+            </div>
+        </nav>
+    );
+}
+
+
+// ----------------------types of the props------------------------
+
+
+// propType Sets the types of the props so that we cant pass any other type and reduce chance of mistake
+Navbar.proptype = { title: PropTypes.string, aboutText: PropTypes.string };
+// isRequired is used to set the mandotry feild without this it will result in error
+Navbar.proptype = { title: PropTypes.string.isRequired, aboutText: PropTypes.string };
+// this will get the default text of the props
+Navbar.defaultProps = {
+    title: "set the title here",
+    aboutText: "Set the about us here "
+}
---
+++ src/module1.mjs
@@ -0,0 +1,9 @@
+const a = "A";
+const b = "B";
+const c = "C";
+const d = "D";
+
+export default d;
+export {a};
+export {b};
+export {c};
